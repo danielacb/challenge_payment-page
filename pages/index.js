66bb4ea711b3a0ce7dbddde922b484e5c62b1253
Daniela C. Barbosa <contato@danielacb.com>
2020-11-25 01:55:54 -0300

    create-next-app and initial setup

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Demo Shop</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Demo Shop</h1>
+    </div>
+  );
+}
